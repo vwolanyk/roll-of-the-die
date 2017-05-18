4b9489f0e1fedc5279421577073e00a99868f4d4
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-18 15:46:28 -0400

    Add Comments to Permutations

--- permutations.rb
+++ permutations.rb
@@ -1,7 +1,13 @@
+# Permutations of Dice Rolls
 
+
+# Creat Arrays for each die
 dice_one = *(1..6)
 
 dice_two = *(1..6)
+
+# Display
+# Nested Iterations Get every permutation
 
 dice_one.each do |numd1|
   dice_two.each {|numd2| puts "#{numd1} #{numd2}"}
