e8fd24ae10643534f1d0aae9e4572806046a14f1
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-18 20:38:10 -0400

    Refactor Odds - Works

--- odds.rb
+++ odds.rb
@@ -3,6 +3,9 @@
 # Empty Hash of Possibilities
 roll_totals = { 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0, 9 => 0, 10 =>0, 11=>0, 12 => 0}
 
+# Counter Variable for total permuitations
+permutations = 0
+
 # GET TOTALS from Every permutation of roll
 #  - Nested Iterations Get every permutation
 #   - Add totals to hash per occurs
@@ -11,20 +14,12 @@ roll_totals = { 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0, 9 => 0, 
     (1..6).each do|numd2|
       total = numd1 + numd2
       roll_totals[total] += 1
+      permutations += 1
     end
   end
 
-# DISPLAY Total Times of Each Occurence
-
-# roll_totals.each{ |possibility, total_occur| puts "#{possibility} occurs #{total_occur} times" }
-#
-# puts roll_totals
 
 # DISPLAY Odds for each Possibility
-puts roll_totals
 
-roll_totals.each do |num,times|
-  # percent =  (times/11) * 100
-  puts " The odds of #{num} coming up are #{((times.to_f/36)*100).round} %"
-
-end
+roll_totals.each {|num,times|
+  puts " The odds of #{num} coming up are #{((times.to_f/permutations)*100).round} %"}
